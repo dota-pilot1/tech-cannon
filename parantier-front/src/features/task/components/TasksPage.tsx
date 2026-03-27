a25f88d8c4d7953b1ec001e1162faddf578fb320
oh hyun seok <hyunsokstar@gmail.com>
2026-03-28 06:12:34 +0900

    fix: 문서 클릭 시 폴더 선택 해제

--- parantier-front/src/features/task/components/TasksPage.tsx
+++ parantier-front/src/features/task/components/TasksPage.tsx
@@ -238,6 +238,7 @@ export default function TasksPage() {
 
   const handlePostClick = (post: TaskPost) => {
     setSelectedPostId(post.id);
+    setSelectedFolderId(null);
     setIsEditing(false);
   };
 
